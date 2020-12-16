6b8d2294096dddef3d7a3d05de74cccfecd9c98e
HTBernard <bernard.hugo.thomas@gmail.com>
2020-12-16 09:25:22 +0100

    Initial commit

---
+++ NQueens.py
@@ -0,0 +1,17 @@
+def glouton_QueenFinder() :
+    pass
+
+def best_QueenFinder() :
+    pass
+
+def all_QueenFinder() :
+    pass
+
+def queensSafe() :
+    pass
+
+def isSolution() :
+    pass
+
+def __repr__() :
+    pass
